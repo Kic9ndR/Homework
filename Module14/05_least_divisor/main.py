cb9aedf36c2a295ba94034ac96e81912e045645b
nikit <fateev_nik@bk.ru>
2022-07-07 14:56:34 +0300

    Наибольший делитель

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+def devider(num):
+    for devider in range(2, num + 2):
+        if num % devider == 0:
+            print('Наименьший делитель, отличный от единицы:', devider)
+            return
+
+
+number = int(input('Введите число: '))
+while number <= 0:
+    print('Неверное значение! Повторите попытку')
+    number = int(input('Введите число: '))
+
+devider(number)
