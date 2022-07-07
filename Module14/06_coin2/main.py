7788a3fff3111822e402855b6f9696b05b1576b6
nikit <fateev_nik@bk.ru>
2022-07-07 15:08:16 +0300

    Поиск монетки

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+def check_coordinate(x, y, r):
+    return (x * x + y * y) ** 0.5 < r
+
+
+print('Введите координаты монетки:')
+coordinate_x = float(input('X: '))
+coordinate_y = float(input('Y: '))
+area = int(input('Введите радиус: '))
+
+if check_coordinate(coordinate_x, coordinate_y, area):
+    print('Монетки в области нет.')
+else:
+    print('Монетка где-то рядом.')
