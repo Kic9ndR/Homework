2ac2dc12f2a5f310ad84a3931f3dc53aa84e1bfb
nikit <fateev_nik@bk.ru>
2022-07-26 18:20:59 +0300

    Решено

--- Module16/01_scary_code/main.py
+++ Module16/01_scary_code/main.py
@@ -1,24 +1,24 @@
-a = [1, 5, 3]
-b = [1, 5, 1, 5]
-c = [1, 3, 1, 5, 3, 3]
-for i in b:
-    a.append(i)
-t = 0
-for i in a:
-    if i == 5:
-        t += 1
-print(t)
-d = []
-for i in a:
-    if i != 5:
-        d.append(i)
-for i in c:
-    d.append(i)
-t = 0
-for i in d:
-    if i == 3:
-        t += 1
-print(t)
-print(d)
+main = [1, 5, 3]
+side_list_fi = [1, 5, 1, 5]
+side_list_se = [1, 3, 1, 5, 3, 3]
 
-# TODO переписать программу
+main.extend(side_list_fi)
+
+count_fives = 0
+count_tiple = 0
+for i in main:
+    if i == 5:
+        count_fives += 1
+        main.remove(5)
+
+main.extend(side_list_se)
+for c in main:
+    if c == 3:
+        count_tiple += 1
+
+print('Результат работы программы:')
+print("Кол-во цифр 5 при первом объединении:", count_fives)
+print('Кол-во цифр 3 при втором объединении:', count_tiple)
+print('Итоговый список:', main)
+
+#Закинуть в main второй список и посчитать кол-во 3-ек + вывести на экран ответ
