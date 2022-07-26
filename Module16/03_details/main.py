181ab200d6b5842360f7750dd2c96cf55f2c9337
nikit <fateev_nik@bk.ru>
2022-07-26 18:37:55 +0300

    Решено

--- Module16/03_details/main.py
+++ Module16/03_details/main.py
@@ -2,4 +2,17 @@ shop = [['каретка', 1200], ['шатун', 1000], ['седло', 300],
         ['педаль', 100], ['седло', 1500], ['рама', 12000],
         ['обод', 2000], ['шатун', 200], ['седло', 2700]]
 
-# TODO здесь писать код
+det_count = 0
+det_cost = 0
+detail = input('Название детали: ')
+
+for i in range(len(shop)):
+        if shop[i][0] == detail.lower():
+                det_count += 1
+                det_cost += shop[i][1]
+print()
+if det_count > 0:
+        print('Кол-во деталей -', det_count)
+        print('Общая стоимость -', det_cost)
+else:
+        print('Товар не найден.')
