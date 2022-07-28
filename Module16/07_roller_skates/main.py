3ee347c2965545b0a204f6055ceabaa2e7071360
nikit <fateev_nik@bk.ru>
2022-07-28 14:40:05 +0300

    Решено

--- Module16/07_roller_skates/main.py
+++ Module16/07_roller_skates/main.py
@@ -1,1 +1,23 @@
-# TODO здесь писать код
+skates = []
+foot_size = []
+count = 0
+
+number = int(input('Кол-во коньков: '))
+for i in range(number):
+    query = 'Размер ' + str(i + 1) + ' пары: '
+    size = int(input(query))
+    skates.append(size)
+
+number = int(input('\nКол-во людей: '))
+for i in range(number):
+    query = 'Размер ноги ' + str(i + 1) + ' человека: '
+    size = int(input(query))
+    foot_size.append(size)
+print()
+
+
+for foot in foot_size:
+    if foot in skates:
+        skates.remove(foot)
+        count += 1
+print('Наибольшее кол-во людей, которые могут взять ролики:', count)
