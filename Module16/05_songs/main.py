c6af993704f980ec14ccbb5da86defe53383d6f3
nikit <fateev_nik@bk.ru>
2022-07-28 13:39:15 +0300

    Решено

--- Module16/05_songs/main.py
+++ Module16/05_songs/main.py
@@ -10,4 +10,27 @@ violator_songs = [
     ['Clean', 5.83]
 ]
 
-# TODO здесь писать код
+
+def count_time_songs(songs):
+    count_time = []
+    summ = 0
+    for new_songs in songs:
+        for i in violator_songs:
+            if i[0] == new_songs:
+                count_time.append(i[1])
+    for summ_minute in count_time:
+        summ += summ_minute
+    print('Общее время звучания песен:', round(summ, 2), 'минуты')
+
+
+count_songs = int(input('Введите кол-во выбираемых песен: '))
+
+songs = []
+for i in range(count_songs):
+    print('Название', str(i + 1) + '-й песни:', end=' ')
+    name = input()
+    songs.append(name)
+
+
+print()
+count_time_songs(songs)
