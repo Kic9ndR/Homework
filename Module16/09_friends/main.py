797260342fb97ce765e50f4bba0882d21491c93d
nikit <fateev_nik@bk.ru>
2022-08-01 14:29:23 +0300

    Решено

--- Module16/09_friends/main.py
+++ Module16/09_friends/main.py
@@ -1,1 +1,27 @@
-# TODO здесь писать код
+def balance_of_debt_receipts():
+    for c in range(count_promissory_notes):
+        print()
+        print(str(c + 1) + '-я расписка')
+        to_whom = int(input('Кому: '))
+        from_whom = int(input('От кого: '))
+        count = int(input('Сколько: '))
+        balance[to_whom - 1] -= count
+        balance[from_whom - 1] += count
+    num = 1
+    print('\nБаланс друзей:')
+    for f in balance:
+        print(str(num) + ':', f)
+        num += 1
+
+
+
+count_friend = int(input('Кол-во друзей: '))
+balance = []
+for i in range(1, count_friend + 1):
+    balance.append(0)
+
+
+count_promissory_notes = int(input('Долговых расписок: '))
+
+
+balance_of_debt_receipts()
