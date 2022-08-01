19020cae82b0d00a6e27c70bccd571c289d26139
nikit <fateev_nik@bk.ru>
2022-08-01 14:59:12 +0300

    Решено

--- Module16/10_simmetrical_seq/main.py
+++ Module16/10_simmetrical_seq/main.py
@@ -1,1 +1,16 @@
-# TODO здесь писать код
+number = int(input('Кол-во чисел: '))
+arr = []
+
+for i in range(number):
+    query = str(i + 1) + ' число: '
+    arr.append(int(input('Число: ')))
+print('\nПоследоваельность:', arr)
+
+counter = 0
+while arr != arr[::-1]:
+    arr.insert(number, arr[counter])
+    counter += 1
+
+
+print('Нужно приписать чисел:', counter)
+print('Сами числа:', arr[:counter][::-1])
