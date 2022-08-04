d02b5060e19ce7df210169214cdd46ccc7234265
nikit <fateev_nik@bk.ru>
2022-08-04 15:46:18 +0300

    Исправил

--- Module16/01_scary_code/main.py
+++ Module16/01_scary_code/main.py
@@ -3,22 +3,16 @@ side_list_fi = [1, 5, 1, 5]
 side_list_se = [1, 3, 1, 5, 3, 3]
 
 main.extend(side_list_fi)
-
-count_fives = 0
-count_tiple = 0
-for i in main:
-    if i == 5:
-        count_fives += 1
-        main.remove(5)
+count_five = main.count(5)
+print('Кол-во цифр 5 при первом объединении:', count_five)
+for _ in range(count_five):
+    main.remove(5)
 
 main.extend(side_list_se)
-for c in main:
-    if c == 3:
-        count_tiple += 1
+count_three = main.count(3)
+print('Кол-во цифр 3 при втором объединении:', count_three)
 
 print('Результат работы программы:')
-print("Кол-во цифр 5 при первом объединении:", count_fives)
-print('Кол-во цифр 3 при втором объединении:', count_tiple)
+print("Кол-во цифр 5 при первом объединении:", count_five)
+print('Кол-во цифр 3 при втором объединении:', count_three)
 print('Итоговый список:', main)
-
-#Закинуть в main второй список и посчитать кол-во 3-ек + вывести на экран ответ
