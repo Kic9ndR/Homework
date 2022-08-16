bc6fb5a883cf8e3262bfcf8f4abfb123b631dd6b
nikit <fateev_nik@bk.ru>
2022-08-16 14:21:35 +0300

    Решено

--- Module17/09_list_of_lists_2/main.py
+++ Module17/09_list_of_lists_2/main.py
@@ -1,4 +1,5 @@
 nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
              [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+new_list = [step_3 for step_1 in nice_list for step_2 in step_1 for step_3 in step_2]
+print('Ответ:', new_list)
