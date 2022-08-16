ae54263f1e6003dd24abb8736f2098586fdfcb65
nikit <fateev_nik@bk.ru>
2022-08-16 12:39:46 +0300

    Решено

--- Module17/06_list_compression/main.py
+++ Module17/06_list_compression/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+import random
+
+count_num = int(input('Кол-во чисел в списке: '))
+num_list = [random.randint(0, 2) for _ in range(count_num)]
+
+print('Список до сжатия:', num_list)
+
+num_list = [i for i in num_list if i > 0]
+print('Список после сжатия:', num_list)
