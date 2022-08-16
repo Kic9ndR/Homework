a8f375d7eaec9aca1e9038d295b72ec501b1938a
nikit <fateev_nik@bk.ru>
2022-08-16 08:23:59 +0300

    Решено

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+vowels_len = ["а", "о", "у", "э", "ы", "я", "ё", "ю", "е", "и"]
+vowels_list = []
+
+
+text = input('Введите текст: ')
+
+for i in text:
+    for vowels in vowels_len:
+        if vowels == i:
+            vowels_list.append(vowels)
+
+print('Список гласных букв:', vowels_list)
+print('Длина списка:', len(vowels_list))
