47584f90fdb0ae1c84aa44d06b74514917a621fe
nikit <fateev_nik@bk.ru>
2022-09-01 15:09:34 +0300

    Решено

--- Module18/02_longest_word/main.py
+++ Module18/02_longest_word/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+text = input('Введите строку: ').split()
+max_len = 0
+max_word = ''
+
+for word in text:
+    if len(word) > max_len:
+        max_word = word
+        max_len = len(word)
+
+print('Самое длинное слово:', max_word)
+print('Длина этого слова:', max_len)
