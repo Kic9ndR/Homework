f7e4420cdbce3963efe674e4fe5cd34015fc976b
nikit <fateev_nik@bk.ru>
2022-09-01 15:27:06 +0300

    Решено

--- Module18/03_files/main.py
+++ Module18/03_files/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+import string
+
+file = input('Название файла: ').lower()
+extention = ('.txt', '.docx')
+if file[0] in string.punctuation:
+    print('Ошибка. Название начинается на один из специальных символов.')
+elif not file.endswith(extention):
+    print('Ошибка: Неверное расширение файла. Ожидалось .txt или .docx.')
+else:
+    print('Файл назван верно')
