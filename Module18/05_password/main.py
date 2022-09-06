9de893b45c0b84f12feb3efc4ceae7a0be9f22f3
nikit <fateev_nik@bk.ru>
2022-09-06 21:03:41 +0300

    Решено

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -1,1 +1,24 @@
-# TODO здесь писать код
+count_num = 0
+count_upper = 0
+len_pass = False
+
+
+while True:
+    password = input('Придумайте пароль: ')
+    if len(password) >= 8:
+        len_pass = True
+        for i in password:
+            if i.isupper() == True:
+                count_upper += 1
+            elif i.isdigit() == True:
+                count_num += 1
+    if count_num >= 3 and count_upper >= 1 and len_pass == True:
+        print('Это надежный пароль!')
+        break
+    else:
+        print('Это ненадежный пароль. Попробуйте еще раз.', end='\n')
+        count_num = 0
+        count_upper = 0
+        len_pass = False
+
+
