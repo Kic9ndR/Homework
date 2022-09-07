598db5e5f1e384a4a0db6f6dd8183b6ceba118fa
nikit <fateev_nik@bk.ru>
2022-09-07 11:36:36 +0300

    Решено

--- Module18/07_ip_adress_2/main.py
+++ Module18/07_ip_adress_2/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+IP = input('Введите IP: ').split('.')
+
+for i in IP:
+    if ',' in i:
+        print('Адрес - это четыре числа, разделенные точками.')
+        break
+    elif i.isdigit() != True:
+        print(i, '- это не целое число.')
+        break
+    elif int(i) > 255:
+        print(i, 'превышает 255.')
+        break
+else:
+    print('IP-адрес корректен.')
