f47c8ceeb7bd69736e677f683a400df7896be969
nikit <fateev_nik@bk.ru>
2022-09-15 10:40:38 +0300

    Решено

--- Module19/05_frequency_hist_2/main.py
+++ Module19/05_frequency_hist_2/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+origin_dict = dict()
+replay = 0
+
+
+text = input('Введите текст: ')
+for i in text:
+    replay += 1
+    if origin_dict.get(i) != None:
+        replay += 1
+    origin_dict[i] = replay
+    replay = 0
+
+print('Оригинальный словарь частот:')
+for key, value in sorted(origin_dict.items()):
+    print(f'{key} : {value}')
