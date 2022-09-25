190ce7c43fe206aa2fc0729f7f2fa94ee8d37dc0
nikit <fateev_nik@bk.ru>
2022-09-25 14:57:09 +0300

    Исправил

--- Module19/05_frequency_hist_2/main.py
+++ Module19/05_frequency_hist_2/main.py
@@ -1,15 +1,18 @@
-origin_dict = dict()
-replay = 0
-
-
 text = input('Введите текст: ')
-for i in text:
-    replay += 1
-    if origin_dict.get(i) != None:
-        replay += 1
-    origin_dict[i] = replay
-    replay = 0
+sym_dict = dict()
+text_dict = dict()
 
 print('Оригинальный словарь частот:')
-for key, value in sorted(origin_dict.items()):
-    print(f'{key} : {value}')
+for sym in text:
+    if sym in sym_dict:
+        sym_dict[sym] += 1
+    else:
+        sym_dict[sym] = 1
+for i_sym in sorted(sym_dict.keys()):
+    print(i_sym, ': ', sym_dict[i_sym], sep='')
+
+print('Инвертированный словарь частот:')
+for i_letter, i_num in sym_dict.items():
+    text_dict.setdefault(i_num, []).append(i_letter)
+for i in text_dict:
+    print(i, ': ', text_dict[i], sep='')
