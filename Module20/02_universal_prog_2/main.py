010e5caaf629038ec9ad1d058385f189a6d886c2
nikit <fateev_nik@bk.ru>
2022-10-06 14:54:52 +0300

    Решено

--- Module20/02_universal_prog_2/main.py
+++ Module20/02_universal_prog_2/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+def is_prime(text):
+    crypto = []
+    for i_num, meaning in enumerate(text):
+        check = True
+        if i_num == 0:
+            check = False
+        for i in range(2, (i_num // 2) + 1):
+            if i_num % i == 0:
+                check = False
+                break
+        if check == True:
+            crypto += str(meaning)
+    return crypto
+
+print(is_prime('О Дивный Новый мир!'))
