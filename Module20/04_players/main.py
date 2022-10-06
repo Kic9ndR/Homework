4696f29b495b6511b7746f3615ff86b5647cbcf5
nikit <fateev_nik@bk.ru>
2022-10-06 16:37:52 +0300

    Решено

--- Module20/04_players/main.py
+++ Module20/04_players/main.py
@@ -4,4 +4,6 @@ players = {
     ("Rob", "Bobbin"): (12, 8, 2)
 }
 
-# TODO здесь писать код
+new_players_tuple = list(i_key + i_value for i_key, i_value in players.items())
+
+print(new_players_tuple)
