56f9bcafb7d7dcc0bcbd65a7fc99afd2a9ef804f
nikit <fateev_nik@bk.ru>
2022-12-02 16:35:25 +0300

    Решено

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+def cycle(num):
+    if num <= 0:
+        return 1
+    else:
+        cycle(num - 1)
+        return print(num)
+
+input_num = int(input('Введите число: '))
+
+cycle(input_num)
