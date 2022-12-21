4d3c945fe77afd936b8494d8a4fccbf74cc7d8ae
nikit <fateev_nik@bk.ru>
2022-12-21 21:29:12 +0300

    Решено

--- Module21/03_fibonacci/main.py
+++ Module21/03_fibonacci/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+def fibonacci(num):
+    if num in (1, 2):
+        return 1
+    return fibonacci(num - 1) + fibonacci(num - 2)
+
+
+num_pos = int(input('Введите позицию числа в ряде Фибоначчи: '))
+print('Число:', fibonacci(num_pos))
