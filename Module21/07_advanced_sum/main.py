6c576fb168529577ca5e9ea3c262a6a5b7e2cc20
nikit <fateev_nik@bk.ru>
2023-03-29 17:39:51 +0300

    Решено

--- Module21/07_advanced_sum/main.py
+++ Module21/07_advanced_sum/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+def func_sum(*args):
+    def total(elements):
+        result = []
+        for i in elements:
+            if isinstance(i, int):
+                result.append(i)
+            else:
+                result.extend(total(i))
+        return result
+    return sum(total(args))
+
+# summ = ([[1, 2, [3]], [1], 3])
+# print('Сумма:', func_sum(summ))
+# summ = (1, 2, 3, 4, 5)
+# print('Сумма:', func_sum(summ))
