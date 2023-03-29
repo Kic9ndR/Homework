c566fed93860f97b6debcb1b57675da349583e1a
nikit <fateev_nik@bk.ru>
2023-03-29 17:17:56 +0300

    Решено

--- Module21/06_deep_copy/main.py
+++ Module21/06_deep_copy/main.py
@@ -1,3 +1,19 @@
+import json
+from copy import deepcopy
+
+
+def find_key(struct, key, meaning):
+    if key in struct:
+        struct[key] = meaning
+        return site
+
+    for sub_struct in struct.values():
+        if isinstance(sub_struct, dict):
+            result = find_key(sub_struct, key, meaning)
+            if result:
+                return site
+
+
 site = {
     'html': {
         'head': {
@@ -10,5 +26,19 @@ site = {
         }
     }
 }
+new_sites = []
 
-# TODO здесь писать код
+number_sites = int(input('Сколько сайтов: '))
+for num in range(number_sites):
+    product_name = input('Введите название продукта для нового сайта: ')
+    key = {'title': f'Куплю/продам {product_name} недорого', 'h2': f'У нас самая низкая цена на {product_name}'}
+    tmp_site = deepcopy(site)
+    for i in key:
+        find_key(tmp_site, i, key[i])
+    new_sites.append({product_name: tmp_site})
+
+    for element in new_sites:
+        for k, v in element.items():
+            print(f'Сайт для {k}:')
+            print('site = ', end='')
+            print(json.dumps(v, indent=4, ensure_ascii=False))
