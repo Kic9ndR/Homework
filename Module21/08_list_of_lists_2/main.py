c01c897985acad74fb4379fd342e78f650b4669c
nikit <fateev_nik@bk.ru>
2023-03-30 12:12:53 +0300

    Решено

--- Module21/08_list_of_lists_2/main.py
+++ Module21/08_list_of_lists_2/main.py
@@ -1,4 +1,13 @@
+from collections.abc import Iterable
+
+def flatten(items: Iterable):
+    for item in items:
+        if isinstance(item, Iterable):
+            yield from flatten(item)
+        else:
+            yield item
+
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+print('Ответ:', list(flatten(nice_list)))
