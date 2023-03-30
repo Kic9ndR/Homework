a0cfcff1b38be3c486db1824bb7a4a893a1f67e4
nikit <fateev_nik@bk.ru>
2023-03-30 12:09:01 +0300

    Решено

--- Module21/08_list_of_lists_2/main.py
+++ Module21/08_list_of_lists_2/main.py
@@ -1,4 +1,10 @@
+def simple_list(lst):
+    if not lst:
+        return []
+    return simple_list(lst[:-1]) + (simple_list(lst[-1]) if not isinstance(lst, list) else simple_list(lst[-1]))
+
+
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
 
-# TODO здесь писать код
+print('Ответ:', simple_list(nice_list))
