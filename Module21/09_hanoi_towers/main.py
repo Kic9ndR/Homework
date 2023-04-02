2b32aa47b02709524091bb16921a0a6c9a9ec373
nikit <fateev_nik@bk.ru>
2023-04-02 20:53:27 +0300

    Решено

--- Module21/09_hanoi_towers/main.py
+++ Module21/09_hanoi_towers/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+def hanoi(amount, start, finish):
+    if amount <= 0:
+        return
+    interim = 6 - start - finish
+    hanoi(amount - 1, start, interim)
+    print(f"Переложить диск {amount} со стержня {start} на стержень {finish}")
+    hanoi(amount - 1, interim, finish)
+
+count = int(input('Введите количество дисков: '))
+first_rod = int(input("Введите стартовый стержень: "))
+hanoi(count, first_rod, 3)
