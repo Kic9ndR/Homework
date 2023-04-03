50513f4459d66f639945edc247bc33073b5ebe6b
nikit <fateev_nik@bk.ru>
2023-04-03 15:17:28 +0300

    Переделал

--- Module21/05_make_function_faster/main.py
+++ Module21/05_make_function_faster/main.py
@@ -1,10 +1,17 @@
-
 def calculating_math_func(data: int):
     if data == 0:
         return 1
     return calculating_math_func(data - 1) * data
 
-number = int(input('Введите число: '))
 
-result = (calculating_math_func(number) // number) ** 10
-print(result)
+dict_nums = {}
+
+
+while True:         #Решил сделать бесконечный цикл, потому-что не хотелось стоп-слово придумывать :)
+    number = int(input('Введите число: '))
+    if number in dict_nums:
+        print(dict_nums[number])
+    else:
+        result = (calculating_math_func(number) // number) ** 10
+        print(result)
+    dict_nums[number] = result
