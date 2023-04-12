e8b049000f7210d33caceadbd1f787dac9919f38
nikit <fateev_nik@bk.ru>
2023-04-12 15:17:30 +0300

    Решено

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+count = 0
+tmp_str = ''
+
+with open('numbers.txt') as file:
+    print('Содержимое файла numbers.txt')
+    for row in file.readlines():
+        print(row, end='')
+        tmp_str = row.strip()
+        if tmp_str != '':
+            count += int(tmp_str)
+
+f = open('answer.txt', 'w')
+f.write(str(count))
+print(f'\nСодержимое файла answer.txt\n{count}')
+f.close()
