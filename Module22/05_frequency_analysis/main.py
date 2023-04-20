213a5c47fac08cce17d0fbdb350ac223681eb975
nikit <fateev_nik@bk.ru>
2023-04-20 18:31:22 +0300

    Решено

--- Module22/05_frequency_analysis/main.py
+++ Module22/05_frequency_analysis/main.py
@@ -1,1 +1,31 @@
-# TODO здесь писать код
+from operator import itemgetter
+
+def letter_dict(string):
+    dict_letters = {}
+    amount_letter = len(string)
+    for i in string:
+        counter = string.count(i)
+        counter /= amount_letter
+        dict_letters[i] = counter
+    letter_ratio(dict_letters)
+    return dict_letters
+
+def letter_ratio(dict):
+    analysis = open('analysis.txt', 'w', encoding='utf-8')
+    sort_dict = sorted(dict.items(), key=itemgetter(1), reverse=True)
+    for key, value in sort_dict:
+        analysis.write(f'{key} {round(value, 3)}\n')
+    analysis.close()
+
+
+text = open('text.txt', 'r', encoding='utf-8')
+pure_str = text.read().strip('.').replace(' ', '').lower()
+text.close()
+text = open('text.txt', 'r', encoding='utf-8')
+print(f'Содержимое файла text.txt:\n{text.read()}\n')
+text.close()
+
+letter_dict(pure_str)
+analysis = open('analysis.txt', 'r', encoding='utf-8')
+print(f'Содержимое файла analysis.txt:\n{analysis.read()}')
+analysis.close()
