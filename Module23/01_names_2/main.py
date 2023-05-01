076d05a9cfa3a17995b2e4a3707b0f7e056d5f6b
nikit <fateev_nik@bk.ru>
2023-05-01 13:15:40 +0300

    Решено

--- Module23/01_names_2/main.py
+++ Module23/01_names_2/main.py
@@ -1,1 +1,17 @@
-# TODO здесь писать код
+count_sim = 0
+broke_line = 0
+
+with open('people.txt', 'r', encoding='utf-8') as people:
+    for i in people:
+        try:
+            broke_line += 1
+            len_name = len(i.strip('\n'))
+            if len_name < 3:
+                raise ValueError
+            else:
+                count_sim += len_name
+        except ValueError:
+            print(f'Ошибка возникла в {broke_line} строке: '
+                  f'менее 3-х символов')
+
+print('Общее количество символов:', count_sim)
