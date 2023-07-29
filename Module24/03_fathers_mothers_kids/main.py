8088bf8b39ed2dc3d19266a7e667268d0c3d045f
Nikita Fateev <fateev_nik@bk.ru>
2023-07-29 20:52:26 +0300

    Решено

--- Module24/03_fathers_mothers_kids/main.py
+++ Module24/03_fathers_mothers_kids/main.py
@@ -6,43 +6,58 @@ class Kid():
         self.age = age      #Должен быть младше минимум на 16 лет
         self.status_calm = status_calm
         self.status_hunger = status_hunger
+    
+    def __str__(self) -> str:
+        state_calm = ('спокоен' if self.status_calm == False else 'раздражен')
+        state_hunger = ('не голоден' if self.status_hunger == False else 'голоден')
+        return f'{self.name} {self.age} лет, он(а) {state_calm} и {state_hunger}'
 
 class Parent():
+
+    childrens = []
 
     def __init__(self, name: str, age: int, list_kids: list) -> None:
         self.name = name
         self.age = age
-        self.list_kids = list_kids
+        for kid in list_kids:
+            if age - kid.age > 16:
+                self.childrens.append(kid)
+            else:
+                print(f'\n{kid.name} не ваш ребенок!\n')
 
     def __str__(self) -> str:       #Информация о родителе
-        return f'Меня зовут {self.name}, у меня {len(self.list_kids)} детей и мне {self.age} лет'
+        return f'Меня зовут {self.name}, у меня {len(self.childrens)} детей и мне {self.age} лет\n'
     
     def feed_baby(self):
-        for name, age, hunger, calm in self.list_kids:
-            if hunger == True:
-                print(f'{name} ребенок голоден, сейчас покормлю его.')
-                hunger = False
+        for hunger in self.childrens:
+            if hunger.status_hunger == True:
+                print(f'Надо покормить {hunger.name}')
+                hunger.status_hunger = False
             else:
-                print("Ребенок не хочет кушать.")
-        return hunger
+                print(f'{hunger.name} не голоден')
+
 
     def calm_child(self):
-        for name, age, hunger, calm in self.list_kids:
-            if calm == True:
-                print(f'{name} беспокоится, сейчас я его успокою')
+        for calm in self.childrens:
+            if calm.status_calm == True:
+                print(f'Надо поиграть с {calm.name}, чтобы он успокоился')
+                calm.status_calm = False
             else:
-                print('Ребенок не беспокоится.')
-        return calm
+                print(f'{calm.name} спокоен')
     
 
-Sofi = Kid("sofi", 10, True, False)
+Sofi = Kid("Sofi", 10, True, False)
 Bob = Kid('Bob', 6, False, True)
-Alex = Kid('Alex', 2, True, True)
+Alex = Kid('Alex', 5, True, True)
 
-kids = [Sofi, Bob, Alex]
-
-mom = Parent('Kate', 30, kids)
+mom = Parent('Kate', 30, [Sofi, Bob, Alex])
 
 print(mom)
+print('{}\n{}\n{}'.format(
+    Sofi, Bob, Alex
+))
 mom.calm_child()
 mom.feed_baby()
+print('{}\n{}\n{}'.format(
+    Sofi, Bob, Alex
+))
